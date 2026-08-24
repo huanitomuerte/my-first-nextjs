ae3380435f2ac88a799abf41ee74df8a170a512a
huanitomuerte <huanitomuerte@gmail.com>
2026-08-25 00:45:02 +0300

    build

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <main style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
       <h1>Моят първи Next.js сайт!</h1>
       <p>Всичко работи перфектно.</p>
+	  <p>99999999999999999999999</p>
     </main>
   );
 }
